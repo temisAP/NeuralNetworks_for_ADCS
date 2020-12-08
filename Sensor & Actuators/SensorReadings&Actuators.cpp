d1e0974e2716d98ba403b5495929f9e11023f8a4
Andrés Pedraza <74715759+temisAP@users.noreply.github.com>
2020-12-08 11:03:09 +0100

    KLK

--- Sensor & Actuators/SensorReadings&Actuators.cpp
+++ Sensor & Actuators/SensorReadings&Actuators.cpp
@@ -35,67 +35,84 @@ No funcionan: - ángulo de giro del giroscopio
     #include <Wire.h>                                 // incluir la libreria incluida de Arduino Wire.h de manera que no interfiera con MPU6050_6Axis_MotionApps20.h
 #endif
 
-/***********************  Define parameters ************************/
-//const int mpuAddress = 0x68;  // Puede ser 0x68 o 0x69
-//MPU6050 mpu(mpuAddress);
-MPU6050 mpu;
 
-long accelX, accelY, accelZ;
 
-float gForceX, gForceY, gForceZ;
-int16_t gyroX, gyroY, gyroZ, gyroRateX, gyroRateY, gyroRateZ, accX, accY, accZ;
-float gyroAngleX, gyroAngleY, gyroAngleZ, accAngleX, accAngleY, accAngleZ; // double //gyroAngleX=0
-float currentAngleX, currentAngleY, currentAngleZ, prevAngleX=0, prevAngleY=0, prevAngleZ=0, error, prevError=0, errorSum=0; //
-unsigned long currTime, prevTime=0, loopTime;
-float dt;
 
-// definir parámetros del DMP // no todos son necesarios
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
 
-// definir Quaternion y vectores para las lecturas del DMP
-Quaternion q;           // [w, x, y, z]
-VectorInt16 aa;         // [x, y, z]
-VectorInt16 aaReal;     // [x, y, z]
-VectorInt16 aaWorld;    // [x, y, z]
-VectorFloat gravity;    // [x, y, z]
-float ypr[3];           // [yaw, pitch, roll]
-
-volatile bool mpuInterrupt = false;
-
-#define sampleTime 0.005
-
-/***********************  Define connections ************************/
-//DMP connection (Sensor) // El MPU6050 se conecta al pin 2 (digital) del Arduino para activar la funcionalidad del DMP
-#define INTERRUPT_PIN 2
-
-// Motor X connections
-#define enA 9
-#define in1 8
-#define in2 7
-// Motor Y connections
-#define enB 3
-#define in3 5
-#define in4 4
-// Motor Z connections
-#define enC 3
-#define in5 5
-#define in6 4
-
-/*********************** Classes ***********************/
-
-void dmpDataReady() {
-    mpuInterrupt = true;
-}
 
 /******************************************************************
-* Void Setup
+* Classes
 ******************************************************************/
-void setup() {
+
+class Sensor{
+
+  public:
+  /***********************  Define connections ************************/
+  //DMP connection (Sensor) // El MPU6050 se conecta al pin 2 (digital) del Arduino para activar la funcionalidad del DMP
+  const int INTERRUPT_PIN = 2;
+
+  /***********************  Define parameters ************************/
+  //const int mpuAddress = 0x68;  // Puede ser 0x68 o 0x69
+  //MPU6050 mpu(mpuAddress);
+  MPU6050 mpu;
+
+  long accelX, accelY, accelZ;
+
+  float gForceX, gForceY, gForceZ;
+  int16_t gyroX, gyroY, gyroZ, gyroRateX, gyroRateY, gyroRateZ, accX, accY, accZ;
+  float gyroAngleX, gyroAngleY, gyroAngleZ, accAngleX, accAngleY, accAngleZ; // double //gyroAngleX=0
+  float currentAngleX, currentAngleY, currentAngleZ, prevAngleX=0, prevAngleY=0, prevAngleZ=0, error, prevError=0, errorSum=0; //
+  unsigned long currTime, prevTime=0, loopTime;
+  float dt;
+
+  // definir parámetros del DMP // no todos son necesarios
+  bool dmpReady = false;  // set true if DMP init was successful
+  uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+  uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+  uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+  uint16_t fifoCount;     // count of all bytes currently in FIFO
+  uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+  // definir Quaternion y vectores para las lecturas del DMP
+  Quaternion q;           // [w, x, y, z]
+  VectorInt16 aa;         // [x, y, z]
+  VectorInt16 aaReal;     // [x, y, z]
+  VectorInt16 aaWorld;    // [x, y, z]
+  VectorFloat gravity;    // [x, y, z]
+  float ypr[3];           // [yaw, pitch, roll]
+
+  volatile bool mpuInterrupt = false;
+
+  const float sampleTime = 0.005;
+
+  /***********************  Define methods ************************/
+
+  Sensor();
+  void readSensor();
+
+};
+
+class Actuator{
+  // Motor X connections
+  const int enA = 9;
+  const int in1 = 8;
+  const int in2 = 7;
+  // Motor Y connections
+  const int enB = 3;
+  const int in3 = 5;
+  const int in4 = 4;
+  // Motor Z connections
+  const int enC = 3;
+  const int in5 = 5;
+  const int in6 = 4;
+
+};
+
+/******************************************************************
+* Constructors
+******************************************************************/
+
+Sensor::Sensor(){
   //
   #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
@@ -118,6 +135,55 @@ void setup() {
   Serial.begin(115200);
   Wire.begin();
 
+  // Iniciar MPU6050
+ Serial.println(F("Initializing I2C devices..."));
+ mpu.initialize();
+ pinMode(INTERRUPT_PIN, INPUT);
+
+ // Comprobar conexion
+ Serial.println(F("Testing device connections..."));
+ Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+ // Iniciar DMP
+ Serial.println(F("Initializing DMP..."));
+ devStatus = mpu.dmpInitialize();
+
+ // Valores de calibracion
+ mpu.setXGyroOffset(24);
+ mpu.setYGyroOffset(-24);
+ mpu.setZGyroOffset(5);
+ mpu.setXAccelOffset(-2833);
+ mpu.setYAccelOffset(-108);
+ mpu.setZAccelOffset(1088);
+
+ // Activar DMP
+ if (devStatus == 0) {
+     Serial.println(F("Enabling DMP..."));
+     mpu.setDMPEnabled(true);
+
+     // Activar interrupcion
+     attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+     mpuIntStatus = mpu.getIntStatus();
+
+     Serial.println(F("DMP ready! Waiting for first interrupt..."));
+     dmpReady = true;
+
+     // get expected DMP packet size for later comparison
+     packetSize = mpu.dmpGetFIFOPacketSize();
+ } else {
+     // ERROR!
+     // 1 = initial memory load failed
+     // 2 = DMP configuration updates failed
+     // (if it's going to break, usually the code will be 1)
+     Serial.print(F("DMP Initialization failed (code "));
+     Serial.print(devStatus);
+     Serial.println(F(")"));
+ }
+
+}
+
+Actuator::Actuator() {
+
   // Set all the motor control pins to outputs
   pinMode(enA, OUTPUT);   // MOTOR X
   pinMode(in1, OUTPUT);   // MOTOR X
@@ -139,65 +205,14 @@ void setup() {
   digitalWrite(in5, LOW);
   digitalWrite(in6, LOW);
 
-   // Iniciar MPU6050
-  Serial.println(F("Initializing I2C devices..."));
-  mpu.initialize();
-  pinMode(INTERRUPT_PIN, INPUT);
-
-  // Comprobar conexion
-  Serial.println(F("Testing device connections..."));
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-  // Iniciar DMP
-  Serial.println(F("Initializing DMP..."));
-  devStatus = mpu.dmpInitialize();
-
-  // Valores de calibracion
-  mpu.setXGyroOffset(24);
-  mpu.setYGyroOffset(-24);
-  mpu.setZGyroOffset(5);
-  mpu.setXAccelOffset(-2833);
-  mpu.setYAccelOffset(-108);
-  mpu.setZAccelOffset(1088);
-
-  // Activar DMP
-  if (devStatus == 0) {
-      Serial.println(F("Enabling DMP..."));
-      mpu.setDMPEnabled(true);
-
-      // Activar interrupcion
-      attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-      mpuIntStatus = mpu.getIntStatus();
-
-      Serial.println(F("DMP ready! Waiting for first interrupt..."));
-      dmpReady = true;
-
-      // get expected DMP packet size for later comparison
-      packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
-      // ERROR!
-      // 1 = initial memory load failed
-      // 2 = DMP configuration updates failed
-      // (if it's going to break, usually the code will be 1)
-      Serial.print(F("DMP Initialization failed (code "));
-      Serial.print(devStatus);
-      Serial.println(F(")"));
-  }
-
 }
 
 /******************************************************************
-* Void Loop
+*  dmpDataRead
 ******************************************************************/
-void loop() {
-  setupMPU();
-  mpu.getAcceleration(&accX, &accY, &accZ);
-  mpu.getRotation(&gyroX, &gyroY, &gyroZ);
-  recordAccelGryoRegisters();
-  complementaryFilter();
-  digitalmotionprocessor();s
-  directionControl();
-  printData();
+
+void dmpDataReady() {
+    mpuInterrupt = true;
 }
 
 /******************************************************************
@@ -229,42 +244,11 @@ angles (left half-plane, y < 0). */
   accAngleY = atan(-accX / sqrt(pow(accY, 2) + pow(accZ, 2)))*RAD_TO_DEG; //accAngleY = atan2(accY, accZ)*RAD_TO_DEG; //   accAngleX = atan2(accY, accZ)*RAD_TO_DEG;
   accAngleZ = atan2(accY, accX)*RAD_TO_DEG; // atan(accY / sqrt(pow(accZ, 2) + pow(accX, 2)))*RAD_TO_DEG; // //  accAngleX = atan2(accY, accZ)*RAD_TO_DEG;
 
-  gyroRateX = map(gyroX, -32776, 32776, -250, 250);
-  gyroRateY = map(gyroY, -32776, 32776, -250, 250);
-  gyroRateZ = map(gyroZ, -32776, 32776, -250, 250);
-
-  //gyroAngleX = gyroAngleX + (float)gyroRateX*loopTime/1000;
-  gyroAngleX = (float)gyroRateX*sampleTime;
-  gyroAngleY = (float)gyroRateY*sampleTime;
-  gyroAngleZ = (float)gyroRateZ*sampleTime;
-
   // Calculate Acceleration
   gForceX = accX / 16388.0;  // 16388 is the value that the accel would show when subject to 1g acceleration
   gForceY = accY / 16388.0;
   gForceZ = accZ / 16388.0;
 
-}
-
-/******************************************************************
-* complementaryFilter
-******************************************************************/
-void complementaryFilter() {
-/*We have two measurements of the angle from two different sources. The measurement from accelerometer gets
- * affected by sudden horizontal movements and the measurement from gyroscope gradually drifts away from actual value.
- * In other words, the accelerometer reading gets affected by short duration signals and the gyroscope reading by long
- * duration signals.  */
-
-/* HPF --> gyroscope, LPF --> accelerometer to filter out the drift and noise from the measurement.
-0.9934 and 0.0066 are filter coefficients for a filter time constant of 0.75s. The low pass filter allows any signal longer
-than this duration to pass through it and the high pass filter allows any signal shorter than this duration to pass through.
-The response of the filter can be tweaked by picking the correct time constant. Lowering the time constant will allow more
-horizontal acceleration to pass through. */
-  dt = (millis() - prevTime) / 1000.0;
-  prevTime = millis();
-
-  currentAngleX = 0.98*(prevAngleX + (gyroX / 131)*dt) + 0.02*accAngleX; // 0.98*(prevAngleX + gyroAngleX) + 0.02*(accAngleX);
-  currentAngleY = 0.98*(prevAngleY + (gyroY / 131)*dt) + 0.02*accAngleY; // 0.98*(prevAngleY + gyroAngleY) + 0.02*(accAngleY);
-  currentAngleZ = 0.98*(prevAngleZ + (gyroZ / 131)*dt) + 0.02*accAngleZ; // 0.98*(prevAngleZ + gyroAngleZ) + 0.02*(accAngleZ);
 }
 
 /******************************************************************
@@ -495,4 +479,18 @@ void printData() {        /* PRINT DATA */
    Serial.println("**************************************************************");
 
    delay(100);
+}
+
+/******************************************************************
+* Void Loop
+******************************************************************/
+void loop() {
+  setupMPU();
+  mpu.getAcceleration(&accX, &accY, &accZ);
+  mpu.getRotation(&gyroX, &gyroY, &gyroZ);
+  recordAccelGryoRegisters();
+  complementaryFilter();
+  digitalmotionprocessor();s
+  directionControl();
+  printData();
 }
